054d4220c026eed25348426682215b46da112b30
Javier Gonzalez <javierggt@yahoo.com>
2024-03-09 09:16:21 -0500

    Make a proto Ska dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,132 @@ import Table from "react-bootstrap/Table";
 
 import "./App.css";
 
+const package_list = {
+  packages: [
+    {
+      owner: "sot",
+      name: "Quaternion",
+      last_tag: "4.3.1",
+      last_tag_date: "2024-02-27T01:55:52Z",
+      test_version: "4.3.1",
+      flight: "4.3.0",
+      matlab: "4.3.0",
+      test_status: "PASS",
+      issues: 8,
+      n_pull_requests: 0,
+      branches: 5,
+    },
+    {
+      owner: "acisops",
+      name: "acis_thermal_check",
+      last_tag: "4.8.1",
+      last_tag_date: "2024-02-29T22:56:40Z",
+      test_version: "4.8.1",
+      flight: "4.8.0",
+      matlab: "4.8.0",
+      test_status: "FAIL",
+      issues: 5,
+      n_pull_requests: 1,
+      branches: 10,
+    },
+    {
+      owner: "sot",
+      name: "chandra_aca",
+      last_tag: "4.45.1",
+      last_tag_date: "2024-02-13T20:21:55Z",
+      test_version: "4.45.1",
+      flight: "4.45.0",
+      matlab: "4.45.0",
+      test_status: "PASS",
+      issues: 13,
+      n_pull_requests: 0,
+      branches: 6,
+    },
+    {
+      owner: "sot",
+      name: "fot-matlab",
+      last_tag: "2.4.0",
+      last_tag_date: "2024-01-22T17:18:06Z",
+      test_version: "",
+      flight: "2.3.0",
+      matlab: "2.4.0",
+      test_status: "",
+      issues: 0,
+      n_pull_requests: 0,
+      branches: 8,
+    },
+    {
+      owner: "sot",
+      name: "proseco",
+      last_tag: "5.12.0",
+      last_tag_date: "2024-01-09T22:24:45Z",
+      test_version: "5.12.0",
+      flight: "5.12.0",
+      matlab: "5.12.0",
+      test_status: "PASS",
+      issues: 42,
+      n_pull_requests: 1,
+      branches: 4,
+    },
+    {
+      owner: "sot",
+      name: "starcheck",
+      last_tag: "14.8.0",
+      last_tag_date: "2024-02-27T01:49:58Z",
+      test_version: "14.8.0",
+      flight: "14.7.0",
+      matlab: "14.7.0",
+      test_status: "PASS",
+      issues: 43,
+      n_pull_requests: 5,
+      branches: 9,
+    },
+    {
+      owner: "sot",
+      name: "taco",
+      last_tag: "4.2.3",
+      last_tag_date: "2023-01-23T20:51:27Z",
+      test_version: "4.2.4.dev2+g12dab81",
+      flight: "4.2.3",
+      matlab: "4.2.3",
+      test_status: "PASS",
+      issues: 3,
+      n_pull_requests: 0,
+      branches: 1,
+    },
+  ],
+  "ska3-flight": "2023.10",
+  "ska3-matlab": "2023.11",
+  time: "2024-03-08T15:01:34.321073",
+};
+
 function App() {
+  const rows = package_list["packages"].map((pkg) => (
+    <tr>
+      <td>{pkg.name}</td>
+      <td>{pkg.owner}</td>
+      <td>{pkg.flight}</td>
+      <td>{pkg.matlab}</td>
+      <td>{pkg.test}</td>
+      <td>{pkg.test_status}</td>
+    </tr>
+  ));
+
   return (
     <>
-      <h1> A Table </h1>
+      <h1> Ska Dashboard </h1>
       <Table bordered hover striped variant="light">
         <thead>
           <tr>
-            <th> Col 1 </th>
-            <th> Col 2 </th>
+            <th>Name</th>
+            <th>Owner</th>
+            <th>Flight</th>
+            <th>Matlab</th>
+            <th>Test</th>
+            <th>Test Status</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <td> Content 1</td>
-            <td> Content 2</td>
-          </tr>
-          <tr>
-            <td> Content 3</td>
-            <td> Content 4</td>
-          </tr>
-        </tbody>
+        <tbody>{rows}</tbody>
       </Table>
     </>
   );
